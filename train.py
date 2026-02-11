3ae360bc79397e6d600e3144ea4882ca1993617b
FedericoCerra <cerra.federico@outlook.it>
2026-02-11 19:45:01 +0100

    format: formatted some comments

--- train.py
+++ train.py
@@ -1,18 +1,15 @@
-# train.py - The "Production" Script
 import joblib
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
 
-# 1. Load Data
 iris = load_iris()
 X, y = iris.data, iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 2. parameters found in exploration.ipynb (the Optuna notebook)
+# parameters found in exploration.ipynb (the Optuna notebook)
 
 BEST_PARAMS = {
     "n_estimators": 44,
@@ -22,11 +19,10 @@ BEST_PARAMS = {
     "max_features": "sqrt"
 }
 
-# 3. Build the Pipeline using those specific numbers
 print(f"Training final model with params: {BEST_PARAMS}...")
 
 model = RandomForestClassifier(
-    **BEST_PARAMS,   # This unpacks the dictionary above
+    **BEST_PARAMS,   
     random_state=42, 
     n_jobs=-1
 )
@@ -36,10 +32,8 @@ pipeline = Pipeline([
     ("rf", model)
 ])
 
-# 4. Train Once
 pipeline.fit(X_train, y_train)
 
-# 5. Verify & Save
 score = pipeline.score(X_test, y_test)
 print(f"Final Accuracy: {score:.4f}")
 
